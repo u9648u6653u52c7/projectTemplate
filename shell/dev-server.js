2cdabc683683118529589c9f8168cb0d436a3e59
Brave <u9648u6653u52c7@gmail.com>
2016-08-08 18:19:47 +0800

    1、更新server配置项

--- shell/dev-server.js
+++ shell/dev-server.js
@@ -1,25 +1,37 @@
 /**
+ * http://webpack.github.io/docs/webpack-dev-server.html
  * @author: Brave <u9648u6653u52c7@gmail.com>
  * @date: 2016-08-06
  */
 
 var webpack = require('webpack');
 var webpackDevServer = require('webpack-dev-server');
-var config = require('../conf');
+var conf = require('../conf');
 var webpackDevConfig = require('../conf/webpack.dev');
 
 var server = new webpackDevServer(webpack(webpackDevConfig), {
   // webpack-dev-server options
-  contentBase: config.dev.contentBase,
-  publicPath: "/assets/",
-  hot: true
+
+  contentBase: conf.dev.www,
+  publicPath: conf.dev.publicPath,
+  hot: true,
+  historyApiFallback: false,
+  compress: true,
+  // proxy: {},  // 服务器代理功能根据需求自行配置
 
   // pass [static options](http://expressjs.com/en/4x/api.html#express.static) to inner express server
 
+  // staticOptions: {},
+
   // webpack-dev-middleware options
+
+  noInfo: false,
+  stats: {
+    colors: true
+  }
 
 });
 
-server.listen(8080);
+server.listen(conf.dev.port);
 
 
