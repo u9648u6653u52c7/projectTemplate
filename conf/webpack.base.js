2ef9398050a04d695105bb856e45b3ef172b586a
Brave <u9648u6653u52c7@gmail.com>
2016-08-09 18:01:10 +0800

    loader校验及参数配置

--- conf/webpack.base.js
+++ conf/webpack.base.js
@@ -6,78 +6,86 @@
 var path = require('path');
 var webpack = require('webpack');
 var webpackMerge = require('webpack-merge');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var conf = require('./index');
 var t = require('../shell/utils');
 
 var config = {
-  entry: t.getEntries(conf.entryFileDir, conf.entryKeyType),
-  output: {
-	path: conf.assetsRoot,
-	publicPath: conf.publicPath,
-	filename: '[name].js',
-	chunkFilename: '[id].chunk.js'
-  },
-  module: {
-	loaders: [
-	  {
-		test: /\.jsx?$/,
-		loader: 'babel',
-		query: {
-		  presets: ['es2015'],
-		  plugins: ['transform-runtime'],
-		  cacheDirectory: true
+    entry: t.getEntries(conf.entryFileDir, conf.entryKeyType),
+	output: {
+		path: conf.assetsRoot,
+		publicPath: conf.publicPath,
+		filename: '[name].js',
+		chunkFilename: '[id].chunk.js'
+	},
+	module: {
+		loaders: [
+		{
+			test: /\.jsx?$/,
+			loader: 'babel',
+			query: {
+			  presets: ['react', 'es2015'],
+			  plugins: ['transform-runtime'],
+			  cacheDirectory: true
+			},
+			include: path.resolve(conf.projectRoot, 'src'),
+			exclude: /node_modules/
 		},
-		include: path.resolve(conf.projectRoot, 'src'),
-		exclude: /node_modules|static/
-	  },
-	  {
-		test: require.resolve('zepto'),
-		loader: 'exports?window.Zepto!script'
-	  },
-	  {
-		test: /\.json$/,
-		loader: 'json'
-	  },
-	  {
-		test: /\.hbs$/,
-		loader: "handlebars-template"
-	  },
-	  {
-		test: /\.html$/,
-		loader: 'html'
-	  },
-	  {
-		test: /\.css$/,
-		loader: ExtractTextPlugin.extract("style", "css")
-	  },
-	  {
-		test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-		loader: 'url',
-		query: {
-		  limit: 10000,
+		{
+			test: /\.json$/,
+			loader: 'json'
+		},
+		{
+			test: /\.hbs$/,
+			loader: "handlebars-template"
+		},
+		{
+			test: /\.html$/,
+			loader: 'html',
+			query: {
+				attrs: ['img:src', 'img:data-src'],
+				interpolate: 'require'
+			}
+		},
+		{
+			test: /\.css$/,
+			loader: ExtractTextPlugin.extract("style", "css")
+		},
+		{
+			test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+			loader: 'url',
+			query: {
+		        limit: 8192,
+				context: conf.entryFileDir,
+				name: '[path][name].[ext]'
+			}
+		},
+		{
+			test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+			loader: 'url',
+			query: {
+			    limit: 8192
+			}
+		},
+		{
+			test: require.resolve('zepto'),
+			loader: 'exports?window.Zepto!script'
 		}
-	  },
-	  {
-		test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-		loader: 'url',
-		query: {
-		  limit: 10000,
-		}
-	  }
 	]
-  },
-  plugins: [
-	// new webpack.optimize.CommonsChunkPlugin("commons", "commons.js"),
-	new ExtractTextPlugin("[name].css")
-  ],
-  resolve: {
-    extensions: ['', '.js', '.jsx', '.json']
-  },
-  node: {
-	fs: "empty" // avoids error messages
-  }
+	},
+	plugins: [
+		// new webpack.optimize.CommonsChunkPlugin("commons", "commons.js"),
+		new ExtractTextPlugin("[name].css"),
+		new HtmlWebpackPlugin()
+    ],
+	resolve: {
+        extensions: ['', '.js', '.jsx', '.json']
+    },
+	node: {
+		fs: "empty" // avoids error messages
+	}
 };
 
 module.exports = webpackMerge(config, {});
