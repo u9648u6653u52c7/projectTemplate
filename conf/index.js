e8eade9569a636b6d99cb4ef91031e072139f4d0
Brave <u9648u6653u52c7@gmail.com>
2016-08-09 17:57:15 +0800

    删除冗余配置

--- conf/index.js
+++ conf/index.js
@@ -3,40 +3,37 @@
  * @date: 2016-08-04
  */
 
-
 var path = require('path');
+var projectRoot = path.resolve(__dirname, '..');
 
 module.exports = {
-  projectRoot: path.resolve(__dirname, '..'),
+	projectRoot: projectRoot,
 
-  /**
-   *  Webpack 默认配置
-   */
+	/**
+	*  Webpack 默认配置
+	*/
 
-  entryFileDir: path.resolve(__dirname, '../src/pages'),
-  entryKeyType: true,  // 入口对象的key值类型：true为path类型，false为文件名
-  assetsRoot: path.resolve(__dirname, '../dist'),
-  publicPath: '/',
+	entryFileDir: path.resolve(projectRoot, 'src/pages'),
+	entryKeyType: true,  // 入口对象的key值类型：true为path类型，false为文件名
+	assetsRoot: path.resolve(projectRoot, 'dist'),
+	publicPath: '/assets/',
 
-  /**
-   *  Webpack 开发配置
-   */
+	/**
+	*  Webpack 开发配置
+	*/
 
-  dev: {
-    entryFileDir: path.resolve(__dirname, '../src'),  // 便于调试组件
-    entryKeyType: true,
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    publicPath: '/assets/',
-    www: path.resolve(__dirname, '../src/'),
-    port: '8080'
-  },
+	dev: {
+		hostname: 'localhost',
+		www: path.resolve(projectRoot, 'dist'),
+		port: '8000'
+	},
 
-  /**
-   *  Webpack 生产配置
-   */
+	/**
+	*  Webpack 生产配置
+	*/
 
-  prod: {
+	prod: {
 
-  }
+	}
 
 };
