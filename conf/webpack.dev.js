7a61bbbfdbd6e6d2cdeac6a62fc1af72d13faf41
Brave <u9648u6653u52c7@gmail.com>
2016-08-09 17:59:59 +0800

    入口对象更改

--- conf/webpack.dev.js
+++ conf/webpack.dev.js
@@ -10,38 +10,38 @@ var openBrowserPlugin = require('open-browser-webpack-plugin');
 var t = require('../shell/utils');
 var conf = require('./index');
 var webpackBaseConfig = require('./webpack.base');
+var entries = webpackBaseConfig.entry;
 
 webpackBaseConfig.entry = null;
 
 module.exports = merge(webpackBaseConfig, {
-  entry: (function () {
-    var entries = t.getEntries(conf.dev.entryFileDir, conf.entryKeyType);
-    for ( var key in entries ) {
-      if ( entries.hasOwnProperty(key) ) {
-        entries[key] = ['webpack-dev-server/client?http://localhost:' + conf.dev.port + '/'
-          , "webpack/hot/dev-server"].concat(entries[key]);
-      }
-    }
-    return entries;
-  })(),
-  module: {
-    preLoaders: [
-      {
-        test: /\.jsx?$/,
-        loader: "eslint",
-        exclude: /node_modules|static/
-      }
-    ]
-  },
-  plugins: [
-    new openBrowserPlugin({
-      url: 'http://localhost:' + conf.dev.port
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  eslint: {
-    configFile: path.resolve(conf.projectRoot, '.eslintrc.js'),
-    formatter: require("eslint-friendly-formatter")
-  },
-  devtool: '#eval-source-map'
+    entry: (function (entries) {
+        for ( var key in entries ) {
+          if ( entries.hasOwnProperty(key) ) {
+            entries[key] = ['webpack-dev-server/client?http://' + conf.dev.hostname + ':' + conf.dev.port + '/'
+              , "webpack/hot/dev-server"].concat(entries[key]);
+          }
+        }
+        return entries;
+    })(entries),
+    module: {
+        preLoaders: [
+            {
+                test: /\.jsx?$/,
+                loader: "eslint",
+                exclude: /node_modules|static/
+            }
+        ]
+    },
+    plugins: [
+        new openBrowserPlugin({
+            url: 'http://' + conf.dev.hostname + ':' + conf.dev.port
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    eslint: {
+        configFile: path.resolve(conf.projectRoot, '.eslintrc.js'),
+        formatter: require("eslint-friendly-formatter")
+    },
+    devtool: '#eval-source-map'
 });
