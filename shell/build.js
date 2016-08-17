6492c870dfa6f86153ccd78b527a9eab6e5dd7a8
Brave <u9648u6653u52c7@gmail.com>
2016-08-17 12:03:25 +0800

    打包脚本benefit from vue-cli

--- shell/build.js
+++ shell/build.js
@@ -2,3 +2,35 @@
  * @author: Brave <u9648u6653u52c7@gmail.com>
  * @date: 2016-08-06
  */
+
+require('shelljs/global');
+
+var ora = require('ora');
+var webpack = require('webpack');
+var config = require('../conf');
+var webpackProdConfig = require('../conf/webpack.prod');
+
+console.log(
+	'  Tip:\n' +
+	'  Built files are meant to be served over an HTTP server.\n' +
+	'  Opening index.html over file:// won\'t work.\n'
+);
+
+var spinner = ora('building for production...');
+spinner.start();
+
+var assetsPath = config.prod.assetsRoot;
+rm('-rf', assetsPath);
+mkdir('-p', assetsPath);
+
+webpack(webpackProdConfig, function (err, stats) {
+	spinner.stop();
+	if (err) throw err;
+	process.stdout.write(stats.toString({
+		colors: true,
+		modules: false,
+		children: false,
+		chunks: false,
+		chunkModules: false
+	}) + '\n');
+});
