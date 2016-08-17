aa136ddf826bd8106ab84a55029a8d097550f9c4
Brave <u9648u6653u52c7@gmail.com>
2016-08-17 12:04:08 +0800

    添加优化插件OccurenceOrderPlugin

--- conf/webpack.dev.js
+++ conf/webpack.dev.js
@@ -56,6 +56,7 @@ var config = {
 			minChunks: Infinity
 		}),
 		new ExtractTextPlugin('css/[name].css', {allChunks: true}),
+		new webpack.optimize.OccurenceOrderPlugin(),
 		new webpack.HotModuleReplacementPlugin(),
 		new openBrowserPlugin({url: 'http://' + conf.dev.hostname + ':' + conf.dev.port})
 	],
