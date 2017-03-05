37ca2ffef866b2bb051cc444a68f2e79384796e0
Brave <u9648u6653u52c7@gmail.com>
2017-03-05 19:26:35 +0800

    删除了些没用的变量; 优化了代码结构;

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -3,15 +3,18 @@
  * @date: 2016-08-04
  */
 
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var baseConfig = require('./index');
-var utils = require('../scripts/utils');
-var hash = utils.generateHashString('hash', baseConfig.hashLength);
+const path = require('path');
+const utils = require('../scripts/utils');
+const baseConfig = require('./index');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const entryFiles = utils.getEntries(baseConfig.entryFileDir, baseConfig.entryFileName);
 
-var config = {
-	entry: utils.getEntries(baseConfig.entryFileDir, baseConfig.entryFileName),
+const config = {
+
+	entry: Object.assign(entryFiles, {
+    vendor: ['zepto', 'lodash']
+  }),
+
 	output: {
 		path: baseConfig.assetsRoot,
 		publicPath: baseConfig.assetsPublicPath,
@@ -19,22 +22,30 @@ var config = {
 		chunkFilename: 'js/[id].chunk.js'
 	},
 
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.min.js',
+      zepto: 'zepto/dist/zepto.min.js',
+      lodash: 'lodash/lodash.min.js',
+      components: path.resolve(baseConfig.projectRoot, 'src/components/')
+    },
+    extensions: ['.js', '.jsx', '.json'],
+    modules: [path.resolve(baseConfig.projectRoot, 'node_modules')]
+  },
+
   module: {
-		noParse: [
-			/lodash\.min\.js/,
-			/vue\.min\.js/
-		],
+		noParse: [/vue/, /lodash/],
+
 		rules: [
-			{
-				test: /\.jsx?$/,
+      {
+				test: /\.jsx$/,
 				loader: 'babel-loader',
 				options: {
-				  presets: ['react', 'es2015'],
+				  presets: ['es2015'],
 				  plugins: ['transform-runtime'],
 				  cacheDirectory: true
 				},
-				include: path.resolve(baseConfig.projectRoot, 'src'),
-				exclude: /node_modules|static/
+				exclude: /node_modules|bower_components/
 			},
 
 			{
@@ -46,16 +57,12 @@ var config = {
 			},
 
 			{
-				test: /\.vue$/,
-				loader: 'vue-loader'
-			},
-
-			{
 				test: /\.html$/,
 				loader: 'html-loader',
 				options: {
 					attrs: ['img:src', 'img:data-src'],
-					interpolate: 'require'
+					interpolate: 'require',
+          minimize: true
 				}
 			},
 
@@ -73,7 +80,7 @@ var config = {
 				options: {
           limit: 8192,
 					context: baseConfig.entryFileDir,
-					name: 'img/[path][name].' + hash + '.[ext]'
+					name: 'imgs/[path][name].[ext]'
 				}
 			},
 
@@ -83,9 +90,14 @@ var config = {
 				options: {
           limit: 8192,
 					context: baseConfig.entryFileDir,
-					name: 'fonts/[path][name].' + hash + '.[ext]'
+					name: 'fonts/[path][name].[ext]'
         }
 			},
+
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
 
       // 有些第三方库不支持模块化, 通过使用script-loader和exports-loaderd导出模块供webpack使用
 
@@ -94,22 +106,8 @@ var config = {
 				use: ['exports-loader?Zepto','script-loader']
 			}
 		]
-	},
-
-	resolve: {
-		modules: [path.resolve(baseConfig.projectRoot, 'node_modules')],
-		alias: {
-			vue: 'vue/dist/vue.min.js',
-			zepto: 'zepto/dist/zepto.min.js',
-			lodash: 'lodash/lodash.min.js',
-			static: path.resolve(baseConfig.projectRoot, 'static/'),
-			components: path.resolve(baseConfig.projectRoot, 'src/components/')
-		},
-    extensions: ['.js', '.jsx', '.json']
-  }
+	}
 
 };
-
-config.entry['vendor'] = ['zepto', 'lodash'];   // 根据需要把vue加进vendor
 
 module.exports = config;
