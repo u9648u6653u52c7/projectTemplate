9a03af428f392502482e4f145782cf5a619df2ce
Brave <u9648u6653u52c7@gmail.com>
2017-03-07 19:57:23 +0800

    js代码分号(semi)规则,提倡使用分号

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
-    "extends": "standard",
-    "plugins": [
-        "standard",
-        "promise"
-    ]
+  "extends": "standard",
+  "plugins": [
+    "standard",
+    "promise"
+  ],
+  "rules": {
+    "semi": [1, "always"]
+  }
 };
